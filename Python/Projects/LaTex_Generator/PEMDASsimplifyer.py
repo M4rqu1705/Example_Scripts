9d7b66c203f192177b6d4f0bb18f0ea57e2fa707
M4rqu1705 <m4rc05.dev@gmail.com>
2019-09-01 23:56:39 -0400

    Changed primitivePomodoro so it would display timer and what phase it is in
    Refactored PEMDASsimplifier.py in order for cleaner, more readable code and implemented a procedure step. Missing step in multiplication and on.

--- Python/Projects/LaTex_Generator/PEMDASsimplifyer.py
+++ Python/Projects/LaTex_Generator/PEMDASsimplifyer.py
@@ -2,20 +2,24 @@
 # -*- coding: utf-8 -*-
 import re
 
-def simplify(expression, variables):
+def simplify(expression, variables, procedure=None):
+    if procedure == None:
+        procedure = []
 
     # Initially parse expression to work with a standarized input
     expression = str(expression).strip()
     expression = ''.join(expression.split(' '))
 
     # Identify which operations are to be done
+    substitute_variable = re.compile(r'[A-Za-z]')
     group_parenthesis = re.compile(r'(?:\(([^\[\]\{\}\(\)]+)\)|\[([^\[\]\{\}\(\)]+)\]|\{([^\[\]\{\}\(\)]+)\})')
-    add_substract = re.compile(r'\+|\-')
-    multiply_coefficient = re.compile(r'(\d+)([A-Za-z](?:(?:\^|\*\*)([A-Za-z])?(?(3)[A-Za-z]+|\d+))?)')
+    exponentiate = re.compile(r'(\w+)(?:\^)(\w+)')
     multiply_divide = re.compile(r'(?<=[^\*])\*(?=[^\*])|\/')
-    exponentiate = re.compile(r'(?:\^|\*\*)')
+    multiply_coefficient = re.compile(r'(\d+)([A-Za-z](?:(?:\^|\*\*)([A-Za-z])?(?(3)[A-Za-z]+|\d+))?)')
+    add_substract = re.compile(r'\+|\-')
 
     operations = {
+            "substitution" : bool(substitute_variable.search(expression)),
             "parenthesis" : bool(group_parenthesis.search(expression)),
             "addition_substraction" : bool(add_substract.search(expression)),
             "coefficient_multiplication": bool(multiply_coefficient.search(expression)),
@@ -26,29 +30,45 @@ def simplify(expression, variables):
     #  print("Expression: {}".format(expression))
     #  print(operations)
 
+    # Substitute variable letters for their actual values inside parens
+    if operations["substitution"]:
+        print("Began substituting variables in {}".format(expression))
+
+        i, expression_length = 0, len(expression)
+        while i < expression_length-1:
+            character = str(expression[i])
+            # If character is valid letter and is in keys
+            if substitute_variable.match(character) and character in variables.keys():
+                value = str(variables[character])
+                # Insert value with surrounding parenthesis
+                expression = expression[:i] + "({})".format(value) + expression[i+1:]
+                # Increase working index and final index
+                expression_length += 2 + len(value); i += 2 + len(value)
+            i+=1
+
+        procedure.append(expression)
+
+        return simplify(expression, variables, procedure)
+
     # Work with parenthesis first. The're the most important
-    if operations["parenthesis"]:
+    elif operations["parenthesis"]:
         print("Began evaluating parenthesis in {}".format(expression))
         # Identify every parenthesis inside expression
-        parenthesis_groups = group_parenthesis.finditer(expression)
-        parenthesis_groups = [parenthesis_group.groups() for parenthesis_group in parenthesis_groups]
-        parenthesis_groups = [y for x in parenthesis_groups for y in x]
-        while parenthesis_groups.count(None) > 0:
-            parenthesis_groups.remove(None)
+        matches = group_parenthesis.findall(expression)
+        matches = [y for x in matches for y in x]   # Flatten 2d list
+        matches = list(filter(None, matches))       # Remove Nones from list
 
-        for i in range(len(parenthesis_groups)):
-            start = expression.find(parenthesis_groups[i])
-            end = start + len(parenthesis_groups[i])
+        # Substitute matches with simplified matches
+        for i in range(len(matches)):
+            start = expression.find(matches[i])
+            end = start + len(matches[i])
             start-=1; end+=1
-            expression = expression[:start] + "<item>{}</item>".format(str(i)) + expression[end:]
+            temp = str(simplify(matches[i], variables, procedure))
+            expression = expression[:start] + temp + expression[end:]
 
-        for i in range(len(parenthesis_groups)):
-            temp = str(simplify(parenthesis_groups[i], variables))
-            expression = expression.replace("<item>{}</item>".format(str(i)), temp)
+        procedure.append(expression)
 
-        output = str(simplify(expression, variables))
-
-        return eval(output)
+        return simplify(expression, variables, procedure)
 
     # Decompose and process expression into polynomial terms
     elif operations["addition_substraction"]:
@@ -56,19 +76,13 @@ def simplify(expression, variables):
         # Divide expression into list of terms
         terms = add_substract.sub(',', expression).split(',')
         # Get operators of matches
-        operators = add_substract.finditer(expression)
-        operators = [operator.group(0) for operator in operators]
-
-        # Substitute variable values for factors
-        for i in range(len(terms)):
-            if str(terms[i]) in variables.keys():
-                terms[i] = str(variables[terms[i]])
+        operators = add_substract.findall(expression)
 
         # Recompose expression
         output = ""
         for i in range(len(operators)):
-            output += str(simplify(terms[i], variables)) + str(operators[i])
-        output += str(simplify(terms[-1], variables))
+            output += str(simplify(terms[i], variables, procedure)) + str(operators[i])
+        output += str(simplify(terms[-1], variables, procedure))
 
         # Return evaluated form
         return eval(output)
@@ -90,8 +104,8 @@ def simplify(expression, variables):
         # Recompose expression
         output = ""
         for i in range(len(operators)):
-            output += str(simplify(factors[i], variables)) + str(operators[i])
-        output += str(simplify(factors[-1], variables))
+            output += str(simplify(factors[i], variables, procedure)) + str(operators[i])
+        output += str(simplify(factors[-1], variables, procedure))
 
         # Return for further simplification
         return eval(output)
@@ -124,8 +138,8 @@ def simplify(expression, variables):
     # Simplify exponents
     elif operations["exponent"]:
         print("Began exponentiating {}".format(expression))
-        # Divide expression into base and exponent
-        base, exponent = exponentiate.sub(',', expression).split(',')
+        # Evaluate base and exponent based on regex captures
+        base, exponent = exponentiate.findall(expression)[0]
 
         # Substitute variable values for factors
         if str(base) in variables.keys():
@@ -137,7 +151,8 @@ def simplify(expression, variables):
 
     # If no operations left to do, simply return the number
     else:
+        #  print(procedure)
         return expression
 
 if __name__ == "__main__":
-    print(simplify("3x^3+2x^2+x-1", {"x":2, "z":3}))
+    print(simplify("{(1+2)*2}*[2+4*(2+5)]", {"x":1, "z":5}))
